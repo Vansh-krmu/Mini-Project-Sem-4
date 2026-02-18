9a4cd0b29726aa0437b51094b71e0067ec7d4318
Kasak Yadav <kasakydv08@gmail.com>
2026-02-18 18:44:22 +0530

    Add AST node classes for number and binary operations

---
+++ ast_nodes.py
@@ -0,0 +1,10 @@
+3. ast_nodes.py (AST Structure)
+class NumberNode:
+    def __init__(self, value):
+        self.value = value
+
+class BinaryOpNode:
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
