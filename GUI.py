77cad287e53990d0664037daa5a24d695c41f1e0
Ayush Bangari <bayush2704@gmail.com>
2026-02-24 00:11:40 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -17,3 +17,23 @@ def ast_to_string(node, indent=""):
         text += ast_to_string(node.left, indent + "  ")
         text += ast_to_string(node.right, indent + "  ")
         return text
+
+class MiniMathApp:
+    def _init_(self, root):
+        self.root = root
+        self.root.title("Mini-Math Interpreter")
+        self.root.geometry("750x500")
+
+        tk.Label(root, text="Mini-Math Interpreter", font=("Arial", 16, "bold")).pack(pady=10)
+
+        tk.Label(root, text="Enter Expression:").pack()
+        self.entry = tk.Entry(root, width=40, font=("Arial", 12))
+        self.entry.pack(pady=5)
+
+        tk.Button(root, text="Evaluate", command=self.calculate).pack(pady=10)
+
+        self.result_label = tk.Label(root, text="Result:", font=("Arial", 12))
+        self.result_label.pack()
+
+        frame = tk.Frame(root)
+        frame.pack(pady=10)
