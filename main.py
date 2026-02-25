e7949a72960ed14f358fa15f01d6c404bb0d21a2
Vansh-krmu <roasty35@gmail.com>
2026-02-25 15:36:50 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+=================================
+  MAIN ENTRY POINT OF THE PROGRAM.
+=================================
+'''
+
+import tkinter as tk
+from gui import MiniMathApp
+
+def main():
+    root = tk.Tk()
+    app = MiniMathApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
