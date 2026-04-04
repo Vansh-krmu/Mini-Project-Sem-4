bb39d74aa772ba2512509262ba5c4cc6247506ee
Kasak Yadav <kasakydv08@gmail.com>
2026-04-04 17:04:54 +0530

    Add new AST node classes for unary, variable, assign, and function

--- ast_nodes.py
+++ ast_nodes.py
@@ -2,8 +2,32 @@ class NumberNode:
     def __init__(self, value):
         self.value = value
 
+
 class BinaryOpNode:
     def __init__(self, left, operator, right):
         self.left = left
         self.operator = operator
         self.right = right
+
+
+class UnaryOpNode:
+    def __init__(self, operator, node):
+        self.operator = operator
+        self.node = node
+
+
+class VariableNode:
+    def __init__(self, name):
+        self.name = name
+
+
+class AssignNode:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class FunctionNode:
+    def __init__(self, func_name, argument):
+        self.func_name = func_name
+        self.argument = argument
