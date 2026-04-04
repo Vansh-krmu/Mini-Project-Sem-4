956f93170156542bd0d00b4e90813ca4457cb5c4
aani72 <suhanit120@gmail.com>
2026-04-04 20:28:19 +0530

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,23 +1,42 @@
-from ast_nodes import NumberNode, BinaryOpNode
-from errors import EvaluationError
+import math
+from ast_nodes import *
+
+variables = {}
 
 def evaluate(node):
     if isinstance(node, NumberNode):
         return node.value
 
+    if isinstance(node, UnaryOpNode):
+        return -evaluate(node.node)
+
     if isinstance(node, BinaryOpNode):
-        left = evaluate(node.left)
-        right = evaluate(node.right)
+        if node.operator == '+':
+            return evaluate(node.left) + evaluate(node.right)
+        if node.operator == '-':
+            return evaluate(node.left) - evaluate(node.right)
+        if node.operator == '*':
+            return evaluate(node.left) * evaluate(node.right)
+        if node.operator == '/':
+            return evaluate(node.left) / evaluate(node.right)
 
-        if node.operator == 'PLUS':
-            return left + right
-        elif node.operator == 'MINUS':
-            return left - right
-        elif node.operator == 'MULTIPLY':
-            return left * right
-        elif node.operator == 'DIVIDE':
-            if right == 0:
-                raise EvaluationError("Division by zero")
-            return left / right
+    if isinstance(node, VariableNode):
+        if node.name in variables:
+            return variables[node.name]
+        raise Exception(f"Undefined variable {node.name}")
 
-    raise EvaluationError("Invalid AST")
+    if isinstance(node, AssignNode):
+        value = evaluate(node.value)
+        variables[node.name] = value
+        return value
+
+    if isinstance(node, FunctionNode):
+        val = evaluate(node.argument)
+        if node.func_name == "sin":
+            return math.sin(val)
+        if node.func_name == "cos":
+            return math.cos(val)
+        if node.func_name == "tan":
+            return math.tan(val)
+
+    raise Exception("Evaluation error")
